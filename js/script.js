c4b6b27cdddb8c81e4f427da26a04189d1d098dd
walter_garcia <waltermbgarcia@hotmail.com>
2018-09-20 15:00:38 -0300

    Some adds
    css improvement off;
    random number generate add;
    random RGB generate add;
    interval to change quote add;

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 // This is an array of objects which properties are quotes.
 let quotes = [
-    { quote: "Life is what happens when you're busy make other plans.", source: "John Lennon", citation: "Beautiful Boy (Darling Boy) Song" },
+    { quote: "Life is what happens when you're busy make other plans.", source: "John Lennon", citation: "Beautiful Boy (Darling Boy) Song", year: "1980" },
     { quote: "The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.", source: "Mark Caine" },
     { quote: "When one door of happiness closes, another opens. But often we look so long at the closed door that we do not see the one which has been opened for us.", source: "Helen Keller" },
     { quote: "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.", source: "Mark Twain" },
@@ -14,6 +14,23 @@ let quotes = [
     { quote: "Life is ten percent what happens to you and ninety percent how you respond to it.", source: "Charles Swindoll" }
 ];
 
+// This function generates whole random numbers.
+function generateRandomNumber() {
+  let randomNumber = Math.floor(Math.random() * 256);
+  return randomNumber;
+}
+
+/* This function gets the random numbers from generateRandomNumber()
+   and generate random RGB colors*/
+function generateRandomRGB() {
+  var randomRgb = '';
+  var mixedColor = generateRandomNumber();
+  var mixedColor2 = generateRandomNumber();
+  var mixedColor3 = generateRandomNumber();
+  randomRgb += 'rgb('+ mixedColor + ','+ mixedColor2 + ',' + mixedColor3 + ')';
+  document.body.style.backgroundColor = randomRgb;
+}
+
 // This function gets random quotes from the quotes array.
 function getRandomQuote ( array ) {
   let randomNumber = Math.floor( Math.random() * array.length );
@@ -21,7 +38,7 @@ function getRandomQuote ( array ) {
   return accessQuote;
   }
 
-// This function receive the getRandomQuote function and prints a random quote from the quotes array.
+// This function receives getRandomQuote() values and prints a random quote from the quotes array.
 function printQuote () {
   let receiveQuote = getRandomQuote( quotes );
   let initHtml = '<p class="quote"> ' + receiveQuote.quote + ' </p>';
@@ -36,9 +53,14 @@ function printQuote () {
   initHtml += "</p>";
 
   document.getElementById('quote-box').innerHTML = initHtml;
+  generateRandomRGB();
 }
 
+/* This line of code repeatedly calls printQuote() every five seconds.
+That way, quotes are changed every five seconds or when buttom is clicked*/
+let changeQuote = window.setInterval(printQuote, 5000);
 
 // This event listener will respond to "Show another quote" button clicks.
-// when user clicks anywhere on the button, the "printQuote" function is called.
+/* This line of code calls the printQuote() when user clicks on the
+"Show Another Quote" button*/
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
