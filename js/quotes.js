2863be37bb5a0c30ab59f5386eb65e971d7ed9dd
Rafael Garcia <rafbgarcia@gmail.com>
2018-09-20 19:41:54 -0400

    Better indentation and code consistency

--- js/quotes.js
+++ js/quotes.js
@@ -1,53 +1,53 @@
 // This is an array of objects which properties are quotes.
-let quotes = [
-    {
-      quote: "Life is what happens when you're busy make other plans.",
-      source: "John Lennon",
-      citation: "Beautiful Boy (Darling Boy) Song",
-      year: "1980"
-    },
-    {
-      quote: "The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.",
-      source: "Mark Caine"
-    },
-    {
-      quote: "When one door of happiness closes, another opens. But often we look so long at the closed door that we do not see the one which has been opened for us.",
-      source: "Helen Keller"
-    },
-    {
-      quote: "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.",
-      source: "Mark Twain"
-    },
-    {
-      quote: "A friend is one that knows you as you are, understands where you have been, accepts what you have became, and still, gently allows you to grow.",
-      source: "William Shakespeare"
-    },
-    {
-      quote: "Great minds discuss ideas; average minds discuss events; small minds discuss people.",
-      source: "Eleanor Roosevelt"
-    },
-    {
-      quote: "A successful man is one who can lay a firm foundation with the bricks others have thrown at him.",
-      source: "David Brinkley"
-    },
-    {
-      quote: "You only live once, but if you do it right, once is enough.",
-      source: "Mae West",
-      tag: "Life"
-    },
-    {
-      quote: "The only impossible journey is the one you never begin.",
-      source: "Anthony Robbins",
-      year: "2005"
-    },
-    {
-      quote: "Life is ten percent what happens to you and ninety percent how you respond to it.",
-      source: "Charles Swindoll"
-    },
-    {
-      quote: "It always seems impossible until it's done.",
-      source: "Nelson Mandela",
-      year: "2015",
-      tag: "Perseverance"
-    }
+const quotes = [
+  {
+    quote: "Life is what happens when you're busy make other plans.",
+    source: "John Lennon",
+    citation: "Beautiful Boy (Darling Boy) Song",
+    year: "1980"
+  },
+  {
+    quote: "The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.",
+    source: "Mark Caine"
+  },
+  {
+    quote: "When one door of happiness closes, another opens. But often we look so long at the closed door that we do not see the one which has been opened for us.",
+    source: "Helen Keller"
+  },
+  {
+    quote: "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.",
+    source: "Mark Twain"
+  },
+  {
+    quote: "A friend is one that knows you as you are, understands where you have been, accepts what you have became, and still, gently allows you to grow.",
+    source: "William Shakespeare"
+  },
+  {
+    quote: "Great minds discuss ideas; average minds discuss events; small minds discuss people.",
+    source: "Eleanor Roosevelt"
+  },
+  {
+    quote: "A successful man is one who can lay a firm foundation with the bricks others have thrown at him.",
+    source: "David Brinkley"
+  },
+  {
+    quote: "You only live once, but if you do it right, once is enough.",
+    source: "Mae West",
+    tag: "Life"
+  },
+  {
+    quote: "The only impossible journey is the one you never begin.",
+    source: "Anthony Robbins",
+    year: "2005"
+  },
+  {
+    quote: "Life is ten percent what happens to you and ninety percent how you respond to it.",
+    source: "Charles Swindoll"
+  },
+  {
+    quote: "It always seems impossible until it's done.",
+    source: "Nelson Mandela",
+    year: "2015",
+    tag: "Perseverance"
+  }
 ];
--- js/script.js
+++ js/script.js
@@ -1,67 +1,92 @@
-// My First Project - A Random Quote Generator
+/**
+ * My First Project - A Random Quote Generator
+ *
+ * @author Walter Marcel
+ */
 
-// This function generates whole random numbers.
+
+/**
+ * Generates whole random numbers.
+ */
 function generateRandomNumber() {
   let randomNumber = Math.floor(Math.random() * 256);
   return randomNumber;
 }
 
-/* This function gets the random numbers from generateRandomNumber()
-   generating random RGB colors and applying them to background */
+/**
+ * Gets the random numbers from generateRandomNumber() generating
+ * random RGB colors and applying them to background
+ */
 function generateRandomRGB() {
   var randomRgb = '';
   var mixedColor = generateRandomNumber();
   var mixedColor2 = generateRandomNumber();
   var mixedColor3 = generateRandomNumber();
-  randomRgb += 'rgb('+ mixedColor + ','+ mixedColor2 + ',' + mixedColor3 + ')';
+  randomRgb += 'rgb(' + mixedColor + ',' + mixedColor2 + ',' + mixedColor3 + ')';
+
   document.body.style.backgroundColor = randomRgb;
 }
 
-// This function gets random quotes from the quotes array in quotes.js file
-function getRandomQuote ( array ) {
+/**
+ * Gets random quotes from the quotes array in quotes.js file
+ */
+function getRandomQuote(array) {
   let randomNumber = Math.floor( Math.random() * array.length );
   let accessQuote = array[randomNumber];
   return accessQuote;
-  }
+}
 
-/* This function receives getRandomQuote() values and prints a random quote
-from the quotes array in quotes.js */
-function printQuote () {
+/**
+ * Receives getRandomQuote() values and prints a random quote
+ * from the quotes array in quotes.js
+ */
+function printQuote() {
   let receiveQuote = getRandomQuote( quotes );
   let initHtml = '<p class="quote"> ' + receiveQuote.quote + ' </p>';
   initHtml += '<p class="source"> ' + receiveQuote.source;
-    if (receiveQuote.hasOwnProperty("citation")) {
-      initHtml += '<span class="citation"> ' + receiveQuote.citation + ' </span>';
-    }
-    if (receiveQuote.hasOwnProperty("year")) {
-      initHtml += '<span class="year"> ' + receiveQuote.year + ' </span>';
-    }
-    if (receiveQuote.hasOwnProperty("tag")) {
-      initHtml += ', ' + '<span class="tag"> ' + receiveQuote.tag + ' </span>'
-    }
+
+  if (receiveQuote.hasOwnProperty("citation")) {
+    initHtml += '<span class="citation"> ' + receiveQuote.citation + ' </span>';
+  }
+  if (receiveQuote.hasOwnProperty("year")) {
+    initHtml += '<span class="year"> ' + receiveQuote.year + ' </span>';
+  }
+  if (receiveQuote.hasOwnProperty("tag")) {
+    initHtml += ', ' + '<span class="tag"> ' + receiveQuote.tag + ' </span>'
+  }
   initHtml += "</p>";
-/* Specific access using DIV ID in index.html file to print the random
-quotes on specific place of the page */
-document.getElementById('quote-box').innerHTML = initHtml;
-// Call the generateRandomRGB function.
-generateRandomRGB();
+
+  // Specific access using DIV ID in index.html file to print the random
+  // quotes on specific place of the page
+  document.getElementById('quote-box').innerHTML = initHtml;
+
+  // Call the generateRandomRGB function.
+  generateRandomRGB();
 }
+
 // This event listener will respond to "Show another quote" button clicks.
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-/* This function repeatedly calls printQuote() every seven seconds when
-the user click on the "Start Auto Quote" button. */
 let autoQuote;
+
+/**
+ * Repeatedly calls printQuote() every seven seconds when
+ * the user click on the "Start Auto Quote" button.
+ */
 function autoQuoteStart () {
   autoQuote = window.setInterval(printQuote, 7000);
 }
+
 // This event listener will respond to "Start Auto Quote" button clicks.
 document.getElementById('startShow').addEventListener("click", autoQuoteStart, false);
 
-/* This function stop autoQuoteStart() by clearing the seven seconds
-interval when the user click on the "Stop Auto Quote" button. */
+/**
+ * Stops autoQuoteStart() by clearing the seven seconds
+ * interval when the user click on the "Stop Auto Quote" button.
+ */
 function autoQuoteStop () {
   window.clearInterval(autoQuote);
 }
+
 // This event listener will respond to "Stop Auto Quote" button clicks.
 document.getElementById('stopShow').addEventListener("click", autoQuoteStop, false);
